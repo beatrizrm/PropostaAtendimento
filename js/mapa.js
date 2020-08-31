7588845382d93c2edc00a64822c741d5737184da
Beatriz Martins <beatrizmartinsrt@gmail.com>
2020-08-31 16:35:01 -0300

    adicionando AREA TAVARES - PATOS

--- js/mapa.js
+++ js/mapa.js
@@ -146,6 +146,46 @@ function Area3() {
 	});
 	
 }
+function Area4() {
+	
+	$.getJSON('js/area4.json', function(pontos) {
+		
+		var latlngbounds = new google.maps.LatLngBounds();
+		
+		$.each(pontos, function(index, ponto) {
+			
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
+				title: ponto.Cidade,
+				icon: 'img/4.png'
+			
+			});
+			
+			var myOptions = {
+				content: "<p>" + ponto.Descricao + "</p>",
+				pixelOffset: new google.maps.Size(-150, 0)
+        	};
+
+			infoBox[ponto.Id] = new InfoBox(myOptions);
+			infoBox[ponto.Id].marker = marker;
+			
+			infoBox[ponto.Id].listener = google.maps.event.addListener(marker, 'click', function (e) {
+				abrirInfoBox(ponto.Id, marker);
+			});
+			
+			markers.push(marker);
+			
+			latlngbounds.extend(marker.position);
+			
+		});
+		
+		var markerCluster = new MarkerClusterer(map, markers);
+		
+		map.fitBounds(latlngbounds);
+		
+	});
+	
+}
 
 function Area5() {
 	
